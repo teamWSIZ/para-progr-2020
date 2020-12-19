61fe54c028b8adaaf3451bdd1943477e922355e9
Piotr Marecki <pm@aimachina.io>
2020-12-19 08:41:52 +0100

    async tests

---
+++ etap8_tests/make_it_simple.py
@@ -0,0 +1,50 @@
+import unittest
+from asyncio import sleep
+from random import randint
+
+
+class Engine:
+    async def wait_a_second(self):
+        await sleep(1)
+
+
+class FakeDbConnection:
+    id: int
+
+    async def connect(self):
+        self.id = randint(1, 100)
+        await sleep(1)
+        print(f'connected; id={self.id}')
+
+
+state = dict()
+state['connection']: FakeDbConnection = None
+
+
+def c() -> FakeDbConnection:
+    return state['connection']
+
+
+class Test(unittest.IsolatedAsyncioTestCase):
+
+    async def asyncSetUp(self):
+        if state['connection'] is None:
+            state['connection'] = FakeDbConnection()
+            await c().connect()
+
+    async def asyncTearDown(self):
+        pass
+
+    async def test_waiting(self):
+        print(f'zaczynamy czekanie; btw -- connection: {c().id}')
+        await Engine().wait_a_second()
+        print('koniec czekania')
+
+    async def test_waiting2(self):
+        print(f'zaczynamy czekanie; btw -- connection: {c().id}')
+        await Engine().wait_a_second()
+        print('koniec czekania')
+
+
+if __name__ == "__main__":
+    unittest.main()
